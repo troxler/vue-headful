0583b01de40064403abd7f654bbca5e4693bba33
troxler <2767948+troxler@users.noreply.github.com>
2018-03-29 20:22:48 +0200

    Refactor code to only have a single watcher
    Replace individual watchers with a single, deep watcher on `$props`. That allows to remove unnecessary code and should optimize performance and memory consumption.

--- src/vue-headful.js
+++ src/vue-headful.js
@@ -3,29 +3,13 @@ import headful from 'headful';
 export default {
     name: 'vue-headful',
     props: Object.keys(headful.props),
-    created() {
-        headful(getPropsData(this));
+    watch: {
+        '$props': {
+            handler: (props) => headful(props),
+            deep: true,
+            immediate: true,
+        },
     },
-    watch: extendPropFunctionsWithPropList(headful.props),
     render() {
     },
 };
-
-/**
- * Extends the prop functions for the watcher with a list of declared props on the component.
- * That is necessary so that, for example, a later change to `lang` does not overwrite `ogLocale`.
- */
-function extendPropFunctionsWithPropList(headfulProps) {
-    return Object.keys(headfulProps).reduce((watchers, propName) => {
-        return Object.assign({}, watchers, {
-            [propName]: function (newPropValue) {
-                // add list of props to avoid overwriting other head properties
-                return headfulProps[propName](newPropValue, getPropsData(this));
-            },
-        });
-    }, {});
-}
-
-function getPropsData(vueInstance) {
-    return vueInstance.$options.propsData;
-}
