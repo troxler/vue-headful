db4aa5ede5052b15a408acc32e46b1b65469218a
troxler <2767948+troxler@users.noreply.github.com>
2019-09-29 19:28:22 +0200

    Handle keep-alive

--- src/vue-headful.js
+++ src/vue-headful.js
@@ -1,17 +1,22 @@
 import headful from 'headful';
 
+const handler = props => headful(getPassedProps(props));
+
 export default {
     name: 'vue-headful',
     props: Object.keys(headful.props),
     watch: {
         '$props': {
-            handler: (props) => headful(getPassedProps(props)),
+            handler,
             deep: true,
             immediate: true,
         },
     },
-    render() {
+    activated() {
+        // required for keep-alive components https://vuejs.org/v2/api/#keep-alive
+        handler(this.$props);
     },
+    render() {},
 };
 
 function getPassedProps(props) {
